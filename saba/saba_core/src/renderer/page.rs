e02bde75b841be21b00f075a4d1c41989a0f5793
2021233 <eadxNM297@gmail.com>
2025-05-16 00:25:39 +0900

    update: HttpResponseからDomツリーを作成する処理を追加

--- saba/saba_core/src/renderer/page.rs
+++ saba/saba_core/src/renderer/page.rs
@@ -1,7 +1,13 @@
+use crate::alloc::string::ToString;
 use crate::browser::Browser;
+use crate::http::HttpResponse;
 use crate::renderer::dom::node::Window;
+use crate::renderer::html::parser::HtmlParser;
+use crate::renderer::html::token::HtmlTokenizer;
+use crate::utils::convert_dom_to_string;
 use alloc::rc::Rc;
 use alloc::rc::Weak;
+use alloc::string::String;
 use core::cell::RefCell;
 
 #[derive(Debug, Clone)]
@@ -11,6 +17,24 @@ pub struct Page {
 }
 
 impl Page {
+    pub fn receive_response(&mut self, response: HttpResponse) -> String {
+        self.create_frame(response.body());
+
+        if let Some(frame) = &self.frame {
+            let dom = frame.borrow().document().clone();
+            let debug = convert_dom_to_string(&Some(dom));
+            return debug;
+        }
+
+        "".to_string()
+    }
+
+    fn create_frame(&mut self, html: String) {
+        let html_tokenizer = HtmlTokenizer::new(html);
+        let frame = HtmlParser::new(html_tokenizer).construct_tree();
+        self.frame = Some(frame);
+    }
+
     pub fn new() -> Self {
         Self {
             browser: Weak::new(),
