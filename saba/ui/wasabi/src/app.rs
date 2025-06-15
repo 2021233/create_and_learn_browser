6a388a0862951c14387a7016e938f279e4e13b5b
negi SA11 <eadxNM297@gmail.com>
2025-06-15 16:28:50 +0900

    update: コンピュータの不具合のため一時コミット

--- saba/ui/wasabi/src/app.rs
+++ saba/ui/wasabi/src/app.rs
@@ -1,16 +1,24 @@
-use alloc::rc::Rc;
-use core::cell::RefCell:
-use noli::window::Window;
-use saba_core::browser::Browser;
 use crate::alloc::string::ToString;
+use alloc::rc::Rc;
+use core::cell::RefCell;
+use noli::error::Result as OsResult;
+use noli::window::Window;
+use noli::windows::StringSize;
+use saba_core::browser::Browser;
+use saba_core::constants::WINDOW_INIt_X_POS;
 use saba_core::constants::WHITE;
 use saba_core::constants::WINDOW_HEIGHT;
-use saba_core::constants::WINDOW_INIt_X_POS;
 use saba_core::constants::WINDOW_INIT_Y_POS;
 use saba_core::constants::WINDOW_WIDTH;
+use saba_core::constants::*;
 
 #[derive(Debug)]
 pub struct WasabiUI {
     browser: Rc<RefCell<Browser>>,
     window: Winsow,
+}
+
+impl WasabiUI {
+    fn setup_toolbar(&mut self) -> OsResult {
+    }
 }
